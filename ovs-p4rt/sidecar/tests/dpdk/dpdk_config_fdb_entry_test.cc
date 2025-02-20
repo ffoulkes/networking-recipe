31670cefe4fc5687b315f4f235a04ebdaa311cde
Derek G Foster <justffoulkes@gmail.com>
2025-02-19 20:40:56 -0800

    [ovsp4rt] Finish dpdk_config_fdb_entry_test (#19)
    - Implemented remaining test cases
    
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/tests/dpdk/dpdk_config_fdb_entry_test.cc
+++ ovs-p4rt/sidecar/tests/dpdk/dpdk_config_fdb_entry_test.cc
@@ -28,39 +28,74 @@ class DpdkConfigFdbEntryTest : public ::testing::Test {
   DpdkConfigFdbEntryTest() {}
   ~DpdkConfigFdbEntryTest() = default;
 
-  struct mac_learning_info learn_info_;
+  void InitFdbTunnelInfo(struct mac_learning_info& learn_info) {
+    constexpr uint8_t MAC_ADDR[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
+
+    memcpy(learn_info.mac_addr, MAC_ADDR, sizeof(learn_info.mac_addr));
+    learn_info.is_tunnel = true;
+  }
+
+  void InitFdbVlanInfo(struct mac_learning_info& learn_info) {
+    constexpr uint8_t MAC_ADDR[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
+    // Note: 8-bit vlan_id.
+    constexpr uint32_t VLAN_ID = 42;
+
+    memcpy(learn_info.mac_addr, MAC_ADDR, sizeof(learn_info.mac_addr));
+    learn_info.vln_info.vlan_id = VLAN_ID;
+    learn_info.is_vlan = true;
+  }
 };
 
-TEST_F(DpdkConfigFdbEntryTest, connectFailure) {
-  TestClientMock client;
+//----------------------------------------------------------------------
+// Setup test cases
+//----------------------------------------------------------------------
 
+/**
+ * Exercises client.connect() error path.
+ */
+TEST_F(DpdkConfigFdbEntryTest, connectFailure) {
+  struct mac_learning_info learn_info = {0};
+
+  TestClientMock client;
   EXPECT_CALL(client, connect).WillOnce(Return(absl::InternalError("connect")));
 
-  auto status = DoConfigFdbEntry(client, learn_info_, INSERT_ENTRY, GRPC_ADDR);
+  auto status = DoConfigFdbEntry(client, learn_info, INSERT_ENTRY, GRPC_ADDR);
 
   ASSERT_FALSE(status.ok());
   ASSERT_TRUE(IsInternal(status) && status.message() == "connect")
       << status.message();
 }
 
+/**
+ * Exercises config.getPipelineConfig() error path.
+ */
 TEST_F(DpdkConfigFdbEntryTest, getPipelineConfigFailure) {
-  TestClientMock client;
+  struct mac_learning_info learn_info = {0};
 
+  TestClientMock client;
   EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
   EXPECT_CALL(client, getPipelineConfig)
       .WillOnce(Return(absl::InternalError("getPipelineConfig")));
 
-  auto status = DoConfigFdbEntry(client, learn_info_, INSERT_ENTRY, GRPC_ADDR);
+  auto status = DoConfigFdbEntry(client, learn_info, INSERT_ENTRY, GRPC_ADDR);
 
   ASSERT_FALSE(status.ok());
   ASSERT_TRUE(IsInternal(status) && status.message() == "getPipelineConfig")
       << status.message();
 }
 
-TEST_F(DpdkConfigFdbEntryTest, configTunnelEntryWriteFailure) {
-  TestClientMock client_mock;
-  ::p4::config::v1::P4Info expected_p4info;
+//----------------------------------------------------------------------
+// Tunnel test cases
+//----------------------------------------------------------------------
 
+/**
+ * Exercises ConfigFdbTunnelTableEntry() error path.
+ */
+TEST_F(DpdkConfigFdbEntryTest, configFdbTunnelEntryWriteFailure) {
+  struct mac_learning_info learn_info = {0};
+  InitFdbTunnelInfo(learn_info);
+
+  ::p4::config::v1::P4Info expected_p4info;
   {
     // Note: return value is stratum::Status, not absl::Status.
     auto status = stratum::ParseProtoFromString(P4INFO_TEXT, &expected_p4info);
@@ -68,62 +103,140 @@ TEST_F(DpdkConfigFdbEntryTest, configTunnelEntryWriteFailure) {
         << "Error parsing P4INFO_TEXT: " << status.error_message();
   }
 
-  // TODO(derek): initialize learn_info_
-
-  EXPECT_CALL(client_mock, connect).WillOnce(Return(absl::OkStatus()));
-  EXPECT_CALL(client_mock, getPipelineConfig)
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
       .WillOnce(
           DoAll(SetArgPointee<0>(expected_p4info), Return(absl::OkStatus())));
-  EXPECT_CALL(client_mock, sendWriteRequest)
+  EXPECT_CALL(client, sendWriteRequest)
       .WillOnce(Return(absl::InternalError("sendWriteRequest")));
 
-  auto status =
-      DoConfigFdbEntry(client_mock, learn_info_, INSERT_ENTRY, GRPC_ADDR);
+  auto status = DoConfigFdbEntry(client, learn_info, INSERT_ENTRY, GRPC_ADDR);
 
   ASSERT_FALSE(status.ok());
   ASSERT_TRUE(IsInternal(status) && status.message() == "sendWriteRequest")
       << status.message();
 }
 
-#if false
+/**
+ * Exercises tunnel configuration happy path.
+ */
+TEST_F(DpdkConfigFdbEntryTest, configFdbTunnelEntryWriteSuccess) {
+  struct mac_learning_info learn_info = {0};
+  InitFdbTunnelInfo(learn_info);
+  ::p4::config::v1::P4Info expected_p4info;
+  {
+    // Note: return value is stratum::Status, not absl::Status.
+    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, &expected_p4info);
+    ASSERT_TRUE(status.ok())
+        << "Error parsing P4INFO_TEXT: " << status.error_message();
+  }
 
-TEST_F(DpdkConfigFdbEntryTest, configTunnelEntryWriteSuccess) {
   TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(
+          DoAll(SetArgPointee<0>(expected_p4info), Return(absl::OkStatus())));
+  EXPECT_CALL(client, sendWriteRequest).WillOnce(Return(absl::OkStatus()));
 
-  EXPECT_CALL(client, connect).WillByDefault(Return(absl::OkStatus()));
+  auto status = DoConfigFdbEntry(client, learn_info, INSERT_ENTRY, GRPC_ADDR);
 
-  auto status = DoConfigFdbEntry(client, learn_info_, INSERT_ENTRY, GRPC_ADDR);
+  ASSERT_TRUE(status.ok()) << status.message();
 }
 
+//----------------------------------------------------------------------
+// Vlan test cases
+//----------------------------------------------------------------------
+
+/**
+ * Exercises ConfigFdbTxVlanTableEntry() failure path.
+ */
 TEST_F(DpdkConfigFdbEntryTest, configVlanTxEntryWriteFailure) {
+  struct mac_learning_info learn_info = {0};
+  InitFdbVlanInfo(learn_info);
+
+  ::p4::config::v1::P4Info expected_p4info;
+  {
+    // Note: return value is stratum::Status, not absl::Status.
+    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, &expected_p4info);
+    ASSERT_TRUE(status.ok())
+        << "Error parsing P4INFO_TEXT: " << status.error_message();
+  }
+
   TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(
+          DoAll(SetArgPointee<0>(expected_p4info), Return(absl::OkStatus())));
+  EXPECT_CALL(client, sendWriteRequest)
+      .WillOnce(Return(absl::InternalError("sendWriteRequest")));
 
-  EXPECT_CALL(client, connect).WillByDefault(Return(absl::OkStatus()));
+  auto status = DoConfigFdbEntry(client, learn_info, INSERT_ENTRY, GRPC_ADDR);
 
-  auto status = DoConfigFdbEntry(client, learn_info_, INSERT_ENTRY, GRPC_ADDR);
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == "sendWriteRequest")
+      << status.message();
 }
 
-TEST_F(DpdkConfigFdbEntryTest, configVlanTxEntryWriteSuccess) {
+/**
+ * Exercises ConfigFdbRxVlanTableEntry() failure path.
+ */
+TEST_F(DpdkConfigFdbEntryTest, configFdbRxVlanRxEntryWriteFailure) {
+  struct mac_learning_info learn_info = {0};
+  InitFdbVlanInfo(learn_info);
+
+  ::p4::config::v1::P4Info expected_p4info;
+  {
+    // Note: return value is stratum::Status, not absl::Status.
+    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, &expected_p4info);
+    ASSERT_TRUE(status.ok())
+        << "Error parsing P4INFO_TEXT: " << status.error_message();
+  }
+
   TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(
+          DoAll(SetArgPointee<0>(expected_p4info), Return(absl::OkStatus())));
+  EXPECT_CALL(client, sendWriteRequest)
+      // ConfigFdbTxVlanTableEntry() succeeds
+      .WillOnce(Return(absl::OkStatus()))
+      // ConfigFdbRxVlanTableEntry() fails
+      .WillOnce(Return(absl::InternalError("sendWriteRequest")));
 
-  EXPECT_CALL(client, connect).WillByDefault(Return(absl::OkStatus()));
+  auto status = DoConfigFdbEntry(client, learn_info, INSERT_ENTRY, GRPC_ADDR);
 
-  auto status = DoConfigFdbEntry(client, learn_info_, INSERT_ENTRY, GRPC_ADDR);
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == "sendWriteRequest")
+      << status.message();
 }
 
-TEST_F(DpdkConfigFdbEntryTest, configVlanRxEntryWriteFailure) {
-  TestClientMock client;
-
-  EXPECT_CALL(client, connect).WillByDefault(Return(absl::OkStatus()));
-
-  auto status = DoConfigFdbEntry(client, learn_info_, INSERT_ENTRY, GRPC_ADDR);
-}
-
+/**
+ * Exercises vlan configuration happy path.
+ */
 TEST_F(DpdkConfigFdbEntryTest, configVlanRxEntryWriteSuccess) {
-  TestClientMock client;
-  auto status = DoConfigFdbEntry(client, learn_info_, INSERT_ENTRY, GRPC_ADDR);
-}
+  struct mac_learning_info learn_info = {0};
+  InitFdbVlanInfo(learn_info);
 
-#endif  // false
+  ::p4::config::v1::P4Info expected_p4info;
+  {
+    // Note: return value is stratum::Status, not absl::Status.
+    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, &expected_p4info);
+    ASSERT_TRUE(status.ok())
+        << "Error parsing P4INFO_TEXT: " << status.error_message();
+  }
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(
+          DoAll(SetArgPointee<0>(expected_p4info), Return(absl::OkStatus())));
+  EXPECT_CALL(client, sendWriteRequest)
+      .WillRepeatedly(Return(absl::OkStatus()));
+
+  auto status = DoConfigFdbEntry(client, learn_info, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_TRUE(status.ok()) << status.message();
+}
 
 }  // namespace ovsp4rt
