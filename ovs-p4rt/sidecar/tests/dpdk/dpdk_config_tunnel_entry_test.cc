c0c03623abf042ca31d1064a014914af58cd47c7
Derek Foster <justffoulkes@gmail.com>
2025-02-20 07:54:51 -0800

    [ovsp4rt] Edited error message text
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/tests/dpdk/dpdk_config_tunnel_entry_test.cc
+++ ovs-p4rt/sidecar/tests/dpdk/dpdk_config_tunnel_entry_test.cc
@@ -62,7 +62,7 @@ class DpdkConfigTunnelEntryTest : public ::testing::Test {
   void InitP4Info(::p4::config::v1::P4Info* p4info) {
     auto status = stratum::ParseProtoFromString(P4INFO_TEXT, p4info);
     EXPECT_TRUE(status.ok())
-        << "Error parsing P4INFO_TEXT: " << status.error_message();
+        << "ParseProtoFromString: " << status.error_message();
   }
 };
 
