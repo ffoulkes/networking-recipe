b98d936478980d89762e44bef9d2122942a86f6c
Derek Foster <justffoulkes@gmail.com>
2025-02-20 19:13:13 -0800

    [ovsp4rt] Define es2k_config_tunnel_src_port_test
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

---
+++ ovs-p4rt/sidecar/tests/es2k/es2k_config_tunnel_src_port_test.cc
@@ -0,0 +1,140 @@
+// Copyright 2025 Derek Foster
+// SPDX-License-Identifier: Apache-2.0
+
+// Unit test for DoConfigSrcPortEntry().
+
+#include <absl/status/status.h>
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <stdint.h>
+
+#include "client/ovsp4rt_test_client_mock.h"
+#include "ovsp4rt/ovs-p4rt.h"
+#include "ovsp4rt_doconfig_int.h"
+#include "p4/config/v1/p4info.pb.h"
+#include "p4info_text.h"
+#include "stratum/lib/utils.h"
+
+using ::testing::DoAll;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+
+namespace ovsp4rt {
+
+constexpr bool INSERT_ENTRY = true;
+constexpr char GRPC_ADDR[] = "172.54.19.3:5678";
+
+class Es2kConfigTunnelSrcPortTest : public ::testing::Test {
+ protected:
+  Es2kConfigTunnelSrcPortTest() {}
+  ~Es2kConfigTunnelSrcPortTest() = default;
+
+  void InitMapInfo(struct src_port_info& port_info) {
+    port_info.bridge_id = 42;
+    port_info.vlan_id = 0x123;
+    port_info.src_port = 0x1066;
+  }
+
+  void InitP4Info(::p4::config::v1::P4Info* p4info) {
+    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, p4info);
+    EXPECT_TRUE(status.ok())
+        << "ParseProtoFromString: " << status.error_message();
+  }
+};
+
+//----------------------------------------------------------------------
+// Setup test cases
+//----------------------------------------------------------------------
+
+/**
+ * Exercises client.connect() error path.
+ */
+TEST_F(Es2kConfigTunnelSrcPortTest, connectFailure) {
+  struct src_port_info port_info = {0};
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::InternalError("connect")));
+
+  auto status =
+      DoConfigTunnelSrcPortEntry(client, port_info, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == "connect")
+      << status.message();
+}
+
+/**
+ * Exercises config.getPipelineConfig() error path.
+ */
+TEST_F(Es2kConfigTunnelSrcPortTest, getPipelineConfigFailure) {
+  struct src_port_info port_info = {0};
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(Return(absl::InternalError("getPipelineConfig")));
+
+  auto status =
+      DoConfigTunnelSrcPortEntry(client, port_info, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == "getPipelineConfig")
+      << status.message();
+}
+
+//----------------------------------------------------------------------
+// Tunnel src_port test cases
+//----------------------------------------------------------------------
+
+/**
+ * Exercises error path.
+ */
+TEST_F(Es2kConfigTunnelSrcPortTest, configTunnelSrcPortFailure) {
+  constexpr char ERROR_MESSAGE[] = "sendWriteRequest";
+
+  struct src_port_info port_info = {0};
+  InitMapInfo(port_info);
+
+  ::p4::config::v1::P4Info expected_p4info;
+  InitP4Info(&expected_p4info);
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(
+          DoAll(SetArgPointee<0>(expected_p4info), Return(absl::OkStatus())));
+  EXPECT_CALL(client, sendWriteRequest)
+      .WillOnce(Return(absl::InternalError(ERROR_MESSAGE)));
+
+  auto status =
+      DoConfigTunnelSrcPortEntry(client, port_info, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == ERROR_MESSAGE)
+      << status.message();
+}
+
+/**
+ * Exercises happy path.
+ */
+TEST_F(Es2kConfigTunnelSrcPortTest, configTunnelSrcPortSuccess) {
+  struct src_port_info port_info = {0};
+  InitMapInfo(port_info);
+
+  ::p4::config::v1::P4Info expected_p4info;
+  InitP4Info(&expected_p4info);
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(
+          DoAll(SetArgPointee<0>(expected_p4info), Return(absl::OkStatus())));
+  EXPECT_CALL(client, sendWriteRequest).WillOnce(Return(absl::OkStatus()));
+
+  auto status =
+      DoConfigTunnelSrcPortEntry(client, port_info, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_TRUE(status.ok()) << status.message();
+}
+
+}  // namespace ovsp4rt
