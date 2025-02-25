c4aa8bec37067e3bedb51ac6814e26f6e814eaa9
Derek Foster <justffoulkes@gmail.com>
2025-02-25 05:20:39 -0800

    Implement BaseMacMapInfoTest
    - Created a base class for unit tests that operate on the
      ip_mac_map_info structure. Modified existing tests to use it.
    
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/tests/es2k/es2k_config_dst_ip_mac_map_test.cc
+++ ovs-p4rt/sidecar/tests/es2k/es2k_config_dst_ip_mac_map_test.cc
@@ -5,50 +5,22 @@
 // Unit test for ConfigDstIpMacMapTableEntry(). [es2k]
 
 #include <absl/status/status.h>
-#include <arpa/inet.h>
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
+#include "base_mac_map_info_test.h"
 #include "client/ovsp4rt_test_client_mock.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_config_int.h"
-#include "p4/config/v1/p4info.pb.h"
-#include "p4info_text.h"
-#include "stratum/lib/utils.h"
 
-using ::testing::InvokeWithoutArgs;
 using ::testing::Return;
 
 namespace ovsp4rt {
 
-constexpr bool INSERT_ENTRY = true;
-constexpr bool REMOVE_ENTRY = false;
-
-class Es2kConfigDstIpMacMapTest : public ::testing::Test {
+class Es2kConfigDstIpMacMapTest : public BaseMacMapInfoTest {
  protected:
   Es2kConfigDstIpMacMapTest() {}
   virtual ~Es2kConfigDstIpMacMapTest() = default;
-
-  void InitIpv4MapInfo(struct ip_mac_map_info map_info) {
-    constexpr uint8_t SRC_MAC[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
-    constexpr char IPV4_SRC_ADDR[] = "10.20.30.40";
-    constexpr int IPV4_PREFIX_LEN = 24;
-
-    memcpy(map_info.src_mac_addr, SRC_MAC, sizeof(map_info.src_mac_addr));
-
-    EXPECT_EQ(inet_pton(AF_INET, IPV4_SRC_ADDR,
-                        &map_info.src_ip_addr.ip.v4addr.s_addr),
-              1)
-        << "Error converting " << IPV4_SRC_ADDR;
-    map_info.src_ip_addr.family = AF_INET;
-    map_info.src_ip_addr.prefix_len = IPV4_PREFIX_LEN;
-  }
-
-  void InitP4Info(::p4::config::v1::P4Info* p4info) {
-    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, p4info);
-    EXPECT_TRUE(status.ok())
-        << "ParseProtoFromString: " << status.error_message();
-  }
 };
 
 TEST_F(Es2kConfigDstIpMacMapTest, configDstIpMacMapWriteFailure) {
