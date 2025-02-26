404f655c1d165f874020e88216ffdf7565c66fbb
Derek Foster <justffoulkes@gmail.com>
2025-02-25 16:19:52 -0800

    Address clang-format issues
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/tests/es2k/es2k_config_encap_table_test.cc
+++ ovs-p4rt/sidecar/tests/es2k/es2k_config_encap_table_test.cc
@@ -16,152 +16,144 @@ class Es2kConfigEncapTableTest : public BaseTunnelInfoTest {
 
 // AF_INET && P4_PORT_VLAN_NATIVE_UNTAGGED && OVS_TUNNEL_VXLAN
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv4VxlanUntagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV4TunnelInfo(tunnel_info);
-	InitVxlanUntagged(tunnel_info);
+  InitV4TunnelInfo(tunnel_info);
+  InitVxlanUntagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV4TunnelInfo(table_entry);
-	CheckVxlanUntagged(table_entry);
+  CheckV4TunnelInfo(table_entry);
+  CheckVxlanUntagged(table_entry);
 }
 
 // AF_INET && P4_PORT_VLAN_NATIVE_UNTAGGED && OVS_TUNNEL_GENEVE
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv4GeneveUntagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV4TunnelInfo(tunnel_info);
-	InitGeneveUntagged(tunnel_info);
+  InitV4TunnelInfo(tunnel_info);
+  InitGeneveUntagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV4TunnelInfo(table_entry);
-	CheckGeneveUntagged(table_entry);
+  CheckV4TunnelInfo(table_entry);
+  CheckGeneveUntagged(table_entry);
 }
 
 //----------------------------------------------------------------------
 
 // AF_INET && P4_PORT_VLAN_NATIVE_TAGGED && OVS_TUNNEL_VXLAN
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv4VxlanTagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV4TunnelInfo(tunnel_info);
-	InitVxlanTagged(tunnel_info);
+  InitV4TunnelInfo(tunnel_info);
+  InitVxlanTagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV4TunnelInfo(table_entry);
-	CheckVxlanTagged(table_entry);
+  CheckV4TunnelInfo(table_entry);
+  CheckVxlanTagged(table_entry);
 }
 
 // AF_INET && P4_PORT_VLAN_NATIVE_TAGGED && OVS_TUNNEL_GENEVE
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv4GeneveTagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV4TunnelInfo(tunnel_info);
-	InitGeneveTagged(tunnel_info);
+  InitV4TunnelInfo(tunnel_info);
+  InitGeneveTagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV4TunnelInfo(table_entry);
-	CheckGeneveTagged(table_entry);
+  CheckV4TunnelInfo(table_entry);
+  CheckGeneveTagged(table_entry);
 }
 
 //----------------------------------------------------------------------
 
 // AF_INET6 && P4_PORT_VLAN_NATIVE_UNTAGGED && OVS_TUNNEL_VXLAN
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv6VxlanUntagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV6TunnelInfo(tunnel_info);
-	InitVxlanUntagged(tunnel_info);
+  InitV6TunnelInfo(tunnel_info);
+  InitVxlanUntagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV6TunnelInfo(table_entry);
-	CheckVxlanUntagged(table_entry);
+  CheckV6TunnelInfo(table_entry);
+  CheckVxlanUntagged(table_entry);
 }
 
 // AF_INET6 && P4_PORT_VLAN_NATIVE_UNTAGGED && OVS_TUNNEL_GENEVE
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv6GeneveUntagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV6TunnelInfo(tunnel_info);
-	InitGeneveUntagged(tunnel_info);
+  InitV6TunnelInfo(tunnel_info);
+  InitGeneveUntagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV6TunnelInfo(table_entry);
-	CheckGeneveUntagged(table_entry);
+  CheckV6TunnelInfo(table_entry);
+  CheckGeneveUntagged(table_entry);
 }
 
 //----------------------------------------------------------------------
 
 // AF_INET6 && P4_PORT_VLAN_NATIVE_TAGGED && OVS_TUNNEL_VXLAN
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv6VxlanTagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV6TunnelInfo(tunnel_info);
-	InitVxlanTagged(tunnel_info);
+  InitV6TunnelInfo(tunnel_info);
+  InitVxlanTagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV6TunnelInfo(table_entry);
-	CheckVxlanTagged(table_entry);
+  CheckV6TunnelInfo(table_entry);
+  CheckVxlanTagged(table_entry);
 }
 
 // AF_INET6 && P4_PORT_VLAN_NATIVE_TAGGED && OVS_TUNNEL_GENEVE
 TEST_F(Es2kConfigEncapTableTest, configEncapIpv6GeneveTagged) {
-	p4::v1::TableEntry table_entry;
+  p4::v1::TableEntry table_entry;
 
   struct tunnel_info tunnel_info = {0};
-	InitV6TunnelInfo(tunnel_info);
-	InitGeneveTagged(tunnel_info);
+  InitV6TunnelInfo(tunnel_info);
+  InitGeneveTagged(tunnel_info);
 
   ::p4::config::v1::P4Info p4info;
   InitP4Info(&p4info);
 
-	Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info,
-														 INSERT_ENTRY);
+  Es2kPrepareEncapTableEntry(&table_entry, tunnel_info, p4info, INSERT_ENTRY);
 
-	CheckV6TunnelInfo(table_entry);
-	CheckGeneveTagged(table_entry);
+  CheckV6TunnelInfo(table_entry);
+  CheckGeneveTagged(table_entry);
 }
 
 }  // namespace ovsp4rt
