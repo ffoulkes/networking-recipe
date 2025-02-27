e72cd03074b7229c6d59cf5636763852e2fb912c
Derek Foster <justffoulkes@gmail.com>
2025-02-27 15:40:57 -0800

    Address clang-format issue
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/newovsp4rt.cc
+++ ovs-p4rt/sidecar/newovsp4rt.cc
@@ -2176,9 +2176,10 @@ absl::StatusOr<::p4::v1::ReadResponse> GetTxAccVsiTableEntry(
 }
 
 // called-by: DoConfigSrcPortEntry (es2k)
-absl::Status ConfigVsiSrcPortTableEntry(
-    ClientInterface& client, const struct src_port_info& sp,
-    const ::p4::config::v1::P4Info& p4info, bool insert_entry) {
+absl::Status ConfigVsiSrcPortTableEntry(ClientInterface& client,
+                                        const struct src_port_info& sp,
+                                        const ::p4::config::v1::P4Info& p4info,
+                                        bool insert_entry) {
   ::p4::v1::WriteRequest write_request;
   ::p4::v1::TableEntry* table_entry;
 
