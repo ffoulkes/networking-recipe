ab8de05a34bc8344e15a024384474c21043f72ef
Derek G Foster <justffoulkes@gmail.com>
2025-02-27 15:49:04 -0800

    Define ConfigVsiSrcPortEntryTest (#60)
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/newovsp4rt.cc
+++ ovs-p4rt/sidecar/newovsp4rt.cc
@@ -2176,9 +2176,10 @@ absl::StatusOr<::p4::v1::ReadResponse> GetTxAccVsiTableEntry(
 }
 
 // called-by: DoConfigSrcPortEntry (es2k)
-absl::Status ConfigureVsiSrcPortTableEntry(
-    ClientInterface& client, const struct src_port_info& sp,
-    const ::p4::config::v1::P4Info& p4info, bool insert_entry) {
+absl::Status ConfigVsiSrcPortTableEntry(ClientInterface& client,
+                                        const struct src_port_info& sp,
+                                        const ::p4::config::v1::P4Info& p4info,
+                                        bool insert_entry) {
   ::p4::v1::WriteRequest write_request;
   ::p4::v1::TableEntry* table_entry;
 
@@ -2566,7 +2567,7 @@ absl::Status DoConfigSrcPortEntry(ClientInterface& client,
   vsi_sp.src_port = host_sp;
   // end of refactoring
 
-  return ConfigureVsiSrcPortTableEntry(client, vsi_sp, p4info, insert_entry);
+  return ConfigVsiSrcPortTableEntry(client, vsi_sp, p4info, insert_entry);
 }
 
 //----------------------------------------------------------------------
