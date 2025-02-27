fb16c5663c288699f9a0b79623f6b3ec9263efab
Derek G Foster <justffoulkes@gmail.com>
2025-02-26 19:24:50 -0800

    Define ConfigFdbTunnelTableEntryTest (#54)
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/ovsp4rt_config_int.h
+++ ovs-p4rt/sidecar/ovsp4rt_config_int.h
@@ -19,6 +19,10 @@ extern absl::Status ConfigFdbRxVlanTableEntry(
     ClientInterface& client, const struct mac_learning_info& learn_info,
     const ::p4::config::v1::P4Info& p4info, bool insert_entry);
 
+absl::Status ConfigFdbTunnelTableEntry(
+    ClientInterface& client, const struct mac_learning_info& learn_info,
+    const ::p4::config::v1::P4Info& p4info, bool insert_entry);
+
 extern absl::Status ConfigFdbTxVlanTableEntry(
     ClientInterface& client, const struct mac_learning_info& learn_info,
     const ::p4::config::v1::P4Info& p4info, bool insert_entry);
