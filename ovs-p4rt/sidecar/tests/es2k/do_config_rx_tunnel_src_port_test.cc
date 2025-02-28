679ac464fcfbd4725ce10710462fccbe71c89267
Derek Foster <justffoulkes@gmail.com>
2025-02-27 19:14:31 -0800

    Define generic DoConfigEntry tests
    - Extracted `ConfigFdbEntry`.
    - Extracted `ConfigIpMacMapEntry`.
    - Extracted `ConfigSrcPortEntry`.
    - Extracted `ConfigTunnelEntry`.
    - Extracted `ConfigTunnelSrcPortEntry`.
    - Extracted `ConfigVlanEntry`.
    
    - Defined a minimal unit test for each DoConfigEntry function.
    
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

---
+++ ovs-p4rt/sidecar/tests/es2k/do_config_rx_tunnel_src_port_test.cc
@@ -0,0 +1,113 @@
+// Copyright 2025 Derek Foster
+// SPDX-License-Identifier: Apache-2.0
+
+// Minimal unit test for DoConfigRxTunnelSrcEntry().
+
+#include <absl/status/status.h>
+#include <arpa/inet.h>
+
+// clang-format off
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+// clang-format on
+
+#include "client/ovsp4rt_test_client_mock.h"
+#include "ovsp4rt/ovs-p4rt.h"
+#include "ovsp4rt_doconfig_int.h"
+#include "p4/config/v1/p4info.pb.h"
+#include "p4info_text.h"
+#include "stratum/lib/utils.h"
+
+using ::testing::DoAll;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+
+namespace ovsp4rt {
+
+constexpr bool INSERT_ENTRY = true;
+constexpr char GRPC_ADDR[] = "1.2.3.4:5678";
+
+class DoConfigRxTunnelSrcPortTest : public ::testing::Test {
+ protected:
+  DoConfigRxTunnelSrcPortTest() {}
+  ~DoConfigRxTunnelSrcPortTest() = default;
+
+  void InitIpv4TunnelInfo(struct tunnel_info& tunnel_info) {
+    constexpr char IPV4_DST_ADDR[] = "192.168.17.5";
+    constexpr int IPV4_PREFIX_LEN = 24;
+
+    EXPECT_EQ(inet_pton(AF_INET, IPV4_DST_ADDR,
+                        &tunnel_info.remote_ip.ip.v4addr.s_addr),
+              1)
+        << "Error converting " << IPV4_DST_ADDR;
+    tunnel_info.remote_ip.family = AF_INET;
+    tunnel_info.remote_ip.prefix_len = IPV4_PREFIX_LEN;
+
+    tunnel_info.vni = 0x123;
+    tunnel_info.src_port = 99;
+  }
+
+  void InitIpv6TunnelInfo(struct tunnel_info& tunnel_info) {
+    constexpr char IPV6_SRC_ADDR[] = "fe80::215:5dff:fefa";
+    constexpr char IPV6_DST_ADDR[] = "fe80::215:192.168.17.5";
+    constexpr int IPV6_PREFIX_LEN = 64;
+
+    EXPECT_EQ(inet_pton(AF_INET6, IPV6_DST_ADDR,
+                        &tunnel_info.remote_ip.ip.v6addr.__in6_u.__u6_addr32),
+              1)
+        << "Error converting " << IPV6_DST_ADDR;
+    tunnel_info.remote_ip.prefix_len = IPV6_PREFIX_LEN;
+
+    tunnel_info.vni = 0x911;
+    tunnel_info.src_port = 0xFADE;
+  }
+
+  void InitP4Info(::p4::config::v1::P4Info* p4info) {
+    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, p4info);
+    EXPECT_TRUE(status.ok())
+        << "ParseProtoFromString: " << status;
+  }
+};
+
+/**
+ * Exercises client.connect() error path.
+ */
+TEST_F(DoConfigRxTunnelSrcPortTest, connectFailure) {
+  constexpr char CONNECT_ERROR[] = "connect";
+
+  struct tunnel_info tunnel_info = {0};
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect)
+      .WillOnce(Return(absl::InternalError(CONNECT_ERROR)));
+
+  auto status =
+      DoConfigRxTunnelSrcEntry(client, tunnel_info, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == CONNECT_ERROR)
+      << status;
+}
+
+/**
+ * Exercises config.getPipelineConfig() error path.
+ */
+TEST_F(DoConfigRxTunnelSrcPortTest, getPipelineConfigFailure) {
+  constexpr char PIPELINE_ERROR[] = "getPipelineConfig";
+
+  struct tunnel_info tunnel_info = {0};
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(Return(absl::InternalError(PIPELINE_ERROR)));
+
+  auto status =
+      DoConfigRxTunnelSrcEntry(client, tunnel_info, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == PIPELINE_ERROR)
+      << status;
+}
+
+}  // namespace ovsp4rt
---
+++ ovs-p4rt/sidecar/tests/es2k/do_config_vlan_entry_test.cc
@@ -0,0 +1,80 @@
+// Copyright 2025 Derek Foster
+// SPDX-License-Identifier: Apache-2.0
+
+// Minimal test for DoConfigVlanEntry().
+
+#include <absl/status/status.h>
+#include <stdint.h>
+
+// clang-format off
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+// clang-format on
+
+#include "client/ovsp4rt_test_client_mock.h"
+#include "ovsp4rt/ovs-p4rt.h"
+#include "ovsp4rt_doconfig_int.h"
+#include "p4/config/v1/p4info.pb.h"
+#include "p4info_text.h"
+#include "stratum/lib/utils.h"
+
+using ::testing::DoAll;
+using ::testing::Return;
+using ::testing::SetArgPointee;
+
+namespace ovsp4rt {
+
+constexpr bool INSERT_ENTRY = true;
+constexpr char GRPC_ADDR[] = "1.2.3.4:5678";
+constexpr uint16_t VLAN_ID = 0x1984;
+
+class DoConfigVlanEntryTest : public ::testing::Test {
+ protected:
+  DoConfigVlanEntryTest() {}
+  virtual ~DoConfigVlanEntryTest() = default;
+
+  void InitP4Info(::p4::config::v1::P4Info* p4info) {
+    auto status = stratum::ParseProtoFromString(P4INFO_TEXT, p4info);
+    EXPECT_TRUE(status.ok())
+        << "ParseProtoFromString: " << status;
+  }
+};
+
+/**
+ * Exercises client.connect() error path.
+ */
+TEST_F(DoConfigVlanEntryTest, connectFailure) {
+  constexpr char CONNECT_ERROR[] = "connect";
+  constexpr uint16_t vlan_id = VLAN_ID;
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect)
+      .WillOnce(Return(absl::InternalError(CONNECT_ERROR)));
+
+  auto status = DoConfigVlanEntry(client, vlan_id, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == CONNECT_ERROR)
+      << status.message();
+}
+
+/**
+ * Exercises config.getPipelineConfig() error path.
+ */
+TEST_F(DoConfigVlanEntryTest, getPipelineConfigFailure) {
+  constexpr char PIPELINE_ERROR[] = "getPipelineConfig";
+  constexpr uint16_t vlan_id = VLAN_ID;
+
+  TestClientMock client;
+  EXPECT_CALL(client, connect).WillOnce(Return(absl::OkStatus()));
+  EXPECT_CALL(client, getPipelineConfig)
+      .WillOnce(Return(absl::InternalError(PIPELINE_ERROR)));
+
+  auto status = DoConfigVlanEntry(client, vlan_id, INSERT_ENTRY, GRPC_ADDR);
+
+  ASSERT_FALSE(status.ok());
+  ASSERT_TRUE(IsInternal(status) && status.message() == PIPELINE_ERROR)
+      << status.message();
+}
+
+}  // namespace ovsp4rt
