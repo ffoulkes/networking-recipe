2784796d4bd7dad8def1c991c411205fe6e72e58
Derek Foster <justffoulkes@gmail.com>
2025-03-04 08:42:56 -0800

    Address clang-format issues
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/tests/es2k/client/es2k_config_rx_tunnel_port_entry_test.cc
+++ ovs-p4rt/sidecar/tests/es2k/client/es2k_config_rx_tunnel_port_entry_test.cc
@@ -41,8 +41,8 @@ TEST_F(Es2kConfigRxTunnelPortEntryTest, configRxTunnelEntryFailure) {
   EXPECT_CALL(client, sendWriteRequest)
       .WillOnce(Return(absl::InternalError(ERROR_MESSAGE)));
 
-  auto status = WriteRxTunnelSrcPortTableEntry(client, tunnel_info, p4info,
-                                                INSERT_ENTRY);
+  auto status =
+      WriteRxTunnelSrcPortTableEntry(client, tunnel_info, p4info, INSERT_ENTRY);
 
   ASSERT_FALSE(status.ok());
   ASSERT_TRUE(IsInternal(status) && status.message() == ERROR_MESSAGE)
@@ -60,8 +60,8 @@ TEST_F(Es2kConfigRxTunnelPortEntryTest, configRxTunnelEntrySuccess) {
   TestClientMock client;
   EXPECT_CALL(client, sendWriteRequest).WillOnce(Return(absl::OkStatus()));
 
-  auto status = WriteRxTunnelSrcPortTableEntry(client, tunnel_info, p4info,
-                                                INSERT_ENTRY);
+  auto status =
+      WriteRxTunnelSrcPortTableEntry(client, tunnel_info, p4info, INSERT_ENTRY);
 
   ASSERT_TRUE(status.ok()) << status;
 }
