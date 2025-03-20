e38bedfd4a30e05c064e934449f516c0b7f3a005
Derek G Foster <justffoulkes@gmail.com>
2025-03-20 12:47:36 -0700

    Rename ovsp4rt_simple_api.cc to ovsp4rt_api_utils.cc (#98)
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/ovsp4rt_api_utils.cc
+++ ovs-p4rt/sidecar/ovsp4rt_api_utils.cc
@@ -1,4 +1,5 @@
-// Copyright 2022-2025 Intel Corporation
+// Copyright 2022-2024 Intel Corporation
+// Copyright 2025 Derek Foster
 // SPDX-License-Identifier: Apache-2.0
 
 // ovsprt C API functions that do not have a C++ implementation.
--- ovs-p4rt/sidecar/ovsp4rt_standard_api.cc
+++ ovs-p4rt/sidecar/ovsp4rt_standard_api.cc
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Standard implementation of the ovsp4rt C API.
-// *** UNDER CONSTRUCTION ***
 
 #include "client/ovsp4rt_client.h"
 #include "ovsp4rt/ovs-p4rt.h"
