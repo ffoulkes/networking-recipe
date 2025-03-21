827798edbb9b550150ba9a85ce39ace433b44495
Derek Foster <justffoulkes@gmail.com>
2025-03-21 13:48:50 -0700

    Manually fix clang-format issues
    - For some unknown reason, clang-format is not sorting #includes
      on my local system, resulting in formatting errors when I push
      my changes to GitHub.
    
      Gave up and edited the files by hand. Pfui.
    
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/tests/es2k/client/read_l2_to_tunnel_v6_entry_test.cc
+++ ovs-p4rt/sidecar/tests/es2k/client/read_l2_to_tunnel_v6_entry_test.cc
@@ -16,8 +16,8 @@
 #include <gmock/gmock.h>
 // clang-format on
 
-#include "mac_learn_info_test.h"
 #include "client/ovsp4rt_test_client_mock.h"
+#include "mac_learn_info_test.h"
 #include "ovsp4rt/ovs-p4rt.h"
 #include "ovsp4rt_config_int.h"
 
