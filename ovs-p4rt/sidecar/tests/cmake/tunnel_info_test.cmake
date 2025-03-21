96167f9158dcae8f34c2262c9d7b9cf5a5b90777
Derek G Foster <justffoulkes@gmail.com>
2025-03-21 12:42:54 -0700

    Partition ovsp4rt test class definitions (#101)
    - Extracted base class library and macro definitions to separate
      cmake files.
    
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

---
+++ ovs-p4rt/sidecar/tests/cmake/tunnel_info_test.cmake
@@ -0,0 +1,47 @@
+# Copyright 2025 Derek Foster
+# SPDX-License-Identifier: Apache-2.0
+
+# Library and macro to define tests that use the BaseTunnelInfoTest
+# base class.
+
+#-----------------------------------------------------------------------
+# ovsp4rt::tunnel_info_test
+#-----------------------------------------------------------------------
+add_library(base_tunnel_info_test STATIC
+  base_tunnel_info_test.cc
+  base_tunnel_info_test.h
+)
+
+target_include_directories(base_tunnel_info_test PUBLIC
+  ${OVSP4RT_INCLUDE_DIR}
+  ${SIDECAR_SOURCE_DIR}
+  ${TESTS_SOURCE_DIR}
+)
+
+target_link_libraries(base_tunnel_info_test PUBLIC
+  ovsp4rt::basic_test
+)
+
+add_library(ovsp4rt::tunnel_info_test ALIAS base_tunnel_info_test)
+
+#-----------------------------------------------------------------------
+# define_tunnel_info_test()
+#-----------------------------------------------------------------------
+macro(define_tunnel_info_test TARGET)
+  add_executable(${TARGET}
+    ${TARGET}.cc
+  )
+
+  set_test_properties(${TARGET})
+
+  target_include_directories(${TARGET} PUBLIC
+    ${TESTS_SOURCE_DIR}
+  )
+
+  target_link_libraries(${TARGET} PUBLIC
+    ovsp4rt::test_client
+    ovsp4rt::tunnel_info_test
+  )
+
+  list(APPEND UNIT_TEST_NAMES ${TARGET})
+endmacro()
