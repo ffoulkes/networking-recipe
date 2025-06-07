1d58c2f7c9c1ebf5372ce90a1ea4418ee48a958d
Derek Foster <justffoulkes@gmail.com>
2025-06-07 06:38:27 -0700

    [ovsp4rt_v3] Extract DoConfigRxTunnelSrcEntry
    Signed-off-by: Derek Foster <justffoulkes@gmail.com>

--- ovs-p4rt/sidecar/core/v3/ovsp4rt_v3.cc
+++ ovs-p4rt/sidecar/core/v3/ovsp4rt_v3.cc
@@ -303,35 +303,6 @@ absl::StatusOr<uint32_t> GetTxAccVsiPort(ClientInterface& client,
   return absl::InternalError("Missing port parameter");
 }
 
-// extracted from WriteRxTunnelSrcPortTableEntry
-void PrepareRxTunnelSrcPortTableEntry(p4::v1::TableEntry* table_entry,
-                                      const struct tunnel_info& tunnel_info,
-                                      const ::p4::config::v1::P4Info& p4info,
-                                      bool insert_entry) {
-  if (tunnel_info.local_ip.family == AF_INET &&
-      tunnel_info.remote_ip.family == AF_INET) {
-    EncodeRxTunnelTableEntry(table_entry, tunnel_info, p4info, insert_entry);
-  } else if (tunnel_info.local_ip.family == AF_INET6 &&
-             tunnel_info.remote_ip.family == AF_INET6) {
-    EncodeV6RxTunnelTableEntry(table_entry, tunnel_info, p4info, insert_entry);
-  }
-}
-
-// called-by: DoConfigRxTunnelSrcEntry (es2k)
-absl::Status WriteRxTunnelSrcPortTableEntry(
-    ClientInterface& client, const struct tunnel_info& tunnel_info,
-    const ::p4::config::v1::P4Info& p4info, bool insert_entry) {
-  ::p4::v1::WriteRequest write_request;
-  ::p4::v1::TableEntry* table_entry;
-
-  table_entry = client.initWriteRequest(&write_request, insert_entry);
-
-  PrepareRxTunnelSrcPortTableEntry(table_entry, tunnel_info, p4info,
-                                   insert_entry);
-
-  return client.sendWriteRequest(write_request);
-}
-
 #endif  // ES2K_TARGET
 
 #if defined(ES2K_TARGET)
@@ -476,30 +447,6 @@ absl::Status DoConfigFdbEntry(ClientInterface& client,
 
   // Update P4 tables.
   return ConfigFdbEntry(client, learn_info, p4info, insert_entry);
-}
-
-//----------------------------------------------------------------------
-// DoConfigRxTunnelSrcEntry (ES2K)
-//----------------------------------------------------------------------
-
-absl::Status DoConfigRxTunnelSrcEntry(ClientInterface& client,
-                                      const struct tunnel_info& tunnel_info,
-                                      bool insert_entry,
-                                      const char* grpc_addr) {
-  absl::Status status;
-
-  // Start a new client session.
-  status = client.connect(grpc_addr);
-  if (!status.ok()) return status;
-
-  // Fetch P4Info object from server.
-  ::p4::config::v1::P4Info p4info;
-  status = client.getPipelineConfig(&p4info);
-  if (!status.ok()) return status;
-
-  // Update P4 tables.
-  return WriteRxTunnelSrcPortTableEntry(client, tunnel_info, p4info,
-                                        insert_entry);
 }
 
 #elif defined(DPDK_TARGET)
